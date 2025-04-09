fc21f40f38c53f26a222ef9e03168789d1d2e2d3
Racsok <oscarmcg02@gmail.com>
2025-04-08 21:36:23 -0500

    Se creo el api, y el metodo get

---
+++ crud_sqlite_pyhton/.ipynb_checkpoints/api-checkpoint.py
@@ -0,0 +1,47 @@
+from flask import jsonify, Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+def create_app(environmet="dev"):
+    app = Flask(__name__)
+    return app
+
+app = create_app()
+
+# Configuración de la base de datos SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base_.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy()
+db.init_app(app)
+
+# Definir el modelo de la base de datos
+class Personaje(db.Model):
+    __tablename__ = 'Personaje'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    edad = db.Column(db.Integer , nullable=False)
+    clase = db.Column(db.String(100), nullable=False)
+
+    def to_dict(self):
+        return {"id": self.id, "nombre": self.nombre, "edad": self.edad, "clase": self.clase}
+
+
+# Ruta para obtener todos los usuarios
+@app.route('/Personajes', methods=['GET'])
+def obtener_persoanjes():
+    personajes = Personaje.query.all()
+    return jsonify([personaje.to_dict() for personaje in personajes])
+
+# Ruta para agregar un usuario
+@app.route('/Personaje', methods=['POST'])
+def agregar_persoanje():
+    data = request.get_json()
+    nuevo_personaje = Personaje(nombre=data['nombre'], edad=data['edad'], clase=data['clase'])
+    db.session.add(nuevo_personaje)
+    db.session.commit()
+    return jsonify(nuevo_personaje.to_dict()), 201
+# Iniciar el servidor
+if __name__ == '__main__':
+    app.run(debug=True)
